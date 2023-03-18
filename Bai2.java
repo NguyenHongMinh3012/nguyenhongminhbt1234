4d8e377461cf137016ccf09d26ea30cf18ef79ea
NguyenHongMinh3012 <125679156+NguyenHongMinh3012@users.noreply.github.com>
2023-03-18 17:20:43 +0700

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,19 @@
+package BTJava;
+
+public class Bai2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		 int n = 10;
+	        if(n%2==0)
+	        {
+	            System.out.println("Đây là số chẵn!");
+	        }
+	        else
+	        {
+	            System.out.println("Đây là số lẻ!");
+	}
+
+}
+}
